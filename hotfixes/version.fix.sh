5a3329bdcf4346854c6522f7f101918d5dd12775
mk-pmb <mk-pmb@users.noreply.github.com>
2023-05-31 00:44:20 +0200

    Rakambda/EditSign feat/auto-version

--- hotfixes/version.fix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# -*- coding: utf-8, tab-width: 2 -*-
-set -o pipefail -o errexit
-cd -- lentic
-PROJ_VER="$(git describe --tags --abbrev=0)"
-echo "Detected project version: '$PROJ_VER'" >&2
-sed -re 's~^(version\s*=\s*).*$~\1'"$PROJ_VER-gha~" -i -- gradle.properties
